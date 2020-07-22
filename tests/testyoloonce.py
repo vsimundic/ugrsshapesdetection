71b8491c57a89d4a53b06f0ad7677999ed1db050
Valentin Simundic <simundicv@gmail.com>
2020-07-22 10:17:06 +0200

    New way to run yolo?

--- tests/testyoloonce.py
+++ tests/testyoloonce.py
@@ -2,7 +2,7 @@ import fcntl
 import os.path, sys
 
 sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
-os.chdir("/home/valentin/FAKS/UGRS_projekt/alexeyAB_darknet/darknet")  # change to where darknet is
+os.chdir("/home/valentin/FAKS/UGRS_projekt/alexeyAB_darknet/darknet/")  # change to where darknet is
 from subprocess import Popen, PIPE
 import select
 import cv2
@@ -15,14 +15,20 @@ cmdline_args = ["./darknet",
                 "yolov3-tiny-prn-obj.cfg",
                 "backup/yolov3-tiny-prn-obj_final.weights",
                 "-dont_show",
-                "-thresh", "0.1",
-                "-ext_output", "<", "frames.txt", ">", "result.txt"
+                "-ext_output",
+                "<",
+                "frames.txt", ">", "result.txt"
+                # "-out",
+                # "result.json", "<", "frames.txt"
+                # "frame0.jpg"
                 ]
 
-process = Popen(cmdline_args, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+cmdline_args_str = "./darknet detector test obj.data yolov3-tiny-prn-obj.cfg backup/yolov3-tiny-prn-obj_final.weights -ext_output -dont_show -out result.json < frames.txt"
+
+process = Popen(cmdline_args_str, stdout=PIPE, stderr=PIPE, shell=True)
 stdout, stderr = process.communicate()
-# print(stdout)
-# print(stderr)
+print(stdout.decode('utf-8'))
+print(stderr.decode('utf-8'))
 end = time.time()
 
 print(end - start)
